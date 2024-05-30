b6480f161689ef854ad75c7907ac36abe4d21bce
Kurt <qaz57123@gmail.com>
2024-05-30 13:34:44 +0800

    chore: reformat main.py

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
-from flask import Flask,redirect,url_for,render_template,request,session,flash
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['UPLOAD_FOLDER'] = '/personalBlog/static/image'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
+
 @app.route('/')
 def home():
-    return redirect(url_for('post.home',style='recommanded_view'))
+    return redirect(url_for('post.home', style='recommanded_view'))
+
 
 from personalBlog.route import auth
+
 app.register_blueprint(auth, url_prefix='/auth')
 
 from personalBlog.route import post
+
 app.register_blueprint(post, url_prefix='/post')
 
 from personalBlog.route import aboutBp
+
 app.register_blueprint(aboutBp, url_prefix='')
 
 from personalBlog import db
+
 db.init_db(app)
 
 from personalBlog.controllers.authController import login_manager
+
 login_manager.init_app(app)
 
 if __name__ == '__main__':
     app.run(port=6002)
-
-
-
-
 
 # @app.route('/index')
 # def index():
