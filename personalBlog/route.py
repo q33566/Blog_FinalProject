831f695148583debd50292128dac75fff702be26
Kurt <qaz57123@gmail.com>
2024-05-30 16:07:52 +0800

    format route.py

--- personalBlog/route.py
+++ personalBlog/route.py
@@ -7,54 +7,68 @@ auth = Blueprint('auth', __name__, template_folder='templates/auth')
 post = Blueprint('post', __name__, template_folder='templates/post')
 aboutBp = Blueprint('about', __name__, template_folder='templates/about')
 
-@aboutBp.route('/about', methods=['GET'])
-def about():
-    return aboutDef()
-
-@aboutBp.route('/editAbout', methods=['GET'])
-def editAboutGet():
-    return editAboutGetDef()
-
-@aboutBp.route('/editAbout', methods=['POST'])
-def editAboutPost():
-    return editAboutPostDef()
 
 @auth.route('/register', methods=['POST', 'GET'])
 def register():
     return registerDef()
 
-@auth.route('/login', methods=['POST','GET'])
+
+@auth.route('/login', methods=['POST', 'GET'])
 def login():
     return loginDef()
+
 
 @auth.route('/logout')
 @login_required
 def logout():
     return logoutDef()
-    
+
+
+@aboutBp.route('/about', methods=['GET'])
+def about():
+    return aboutDef()
+
+
+@aboutBp.route('/editAbout', methods=['GET'])
+def editAboutGet():
+    return editAboutGetDef()
+
+
+@aboutBp.route('/editAbout', methods=['POST'])
+def editAboutPost():
+    return editAboutPostDef()
+
+
+
 @post.route('/home/<string:style>', methods=['POST', 'GET'])
 def home(style):
     return homeDef(style)
+
 
 @post.route('/article', methods=['POST', 'GET'])
 def article():
     return articleDef()
 
+
 @post.route('/edit', methods=['POST', 'GET'])
 def edit():
     return editDef()
+
 
 @post.route('/editarticle/<int:id>', methods=['POST', 'GET'])
 def editarticle(id):
     return editarticleDef(id)
 
+
 @post.route('/deletearticle/<int:id>', methods=['POST', 'GET'])
 def deletearticle(id):
     return deletearticleDef(id)
 
+
 @post.route('/articleview/<int:id>', methods=['POST', 'GET'])
 def articleview(id):
     return articleViewDef(id)
+
 
 @post.route('/tagview/<string:tag>', methods=['POST', 'GET'])
 def tagview(tag):
